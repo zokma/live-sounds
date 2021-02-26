22a86139b2deb2a661033a68c4644ef4e68073c7
Zok MADAO <74968358+zokma@users.noreply.github.com>
2021-02-27 00:33:40 +0900

    :hamster: Add AudioDevice class to manage WASAPI, Wave, DirectSound and ASIO devices.
    In current version, only Audio render devices can be enumerated.
    An `AudioDevice.GetAudioDevices(AudioDataFlow.Capture)` returns Empty list for now.

---
+++ LiveSoundsSolution/Zokma.Libs.Tests/UnitTestAudio.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Xunit;
+using Xunit.Abstractions;
+using Zokma.Libs.Audio;
+
+namespace Zokma.Libs.Tests
+{
+    public class UnitTestAudio
+    {
+        private readonly ITestOutputHelper output;
+
+        public UnitTestAudio(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Theory]
+        [InlineData("TestData/AudioFiles/GitExclude/Test01.mp3", true,  1.0f)]
+        [InlineData("TestData/AudioFiles/GitExclude/Test01.mp3", true,  0.5f)]
+        [InlineData("TestData/AudioFiles/GitExclude/Test01.mp3", false, 1.0f)]
+        [InlineData("TestData/AudioFiles/GitExclude/Test01.mp3", false, 0.5f)]
+        public void TestLoadAudioFiles(string path, bool isCached, float volume)
+        {
+            var data = AudioData.LoadAudio(Pathfinder.FindPathName(path), isCached, volume);
+
+            Assert.Equal(isCached, data.IsCached);
+            Assert.Equal(volume,   data.Volume);
+
+            Assert.NotNull(data.WaveFormat);
+
+            output.WriteLine("AverageBytesPerSecond: {0}", data.WaveFormat.AverageBytesPerSecond);
+            output.WriteLine("BitsPerSample: {0}",         data.WaveFormat.BitsPerSample);
+            output.WriteLine("BlockAlign: {0}",            data.WaveFormat.BlockAlign);
+            output.WriteLine("Channels: {0}",              data.WaveFormat.Channels);
+            output.WriteLine("Encoding: {0}",              data.WaveFormat.Encoding);
+            output.WriteLine("ExtraSize: {0}",             data.WaveFormat.ExtraSize);
+            output.WriteLine("SampleRate: {0}",            data.WaveFormat.SampleRate);
+        }
+
+        [Theory]
+        [InlineData("TestData/AudioFiles/GitExclude/Test01.mp3", true,  100.0f, 1.0f)]
+        [InlineData("TestData/AudioFiles/GitExclude/Test01.mp3", false, 100.0f, 1.0f)]
+        [InlineData("TestData/AudioFiles/GitExclude/Test01.mp3", true,  -10.0f, 0.0f)]
+        [InlineData("TestData/AudioFiles/GitExclude/Test01.mp3", false, -10.0f, 0.0f)]
+        public void TestLoadAudioFilesVol(string path, bool isCached, float volume, float expectedVol)
+        {
+            var data = AudioData.LoadAudio(Pathfinder.FindPathName(path), isCached, volume);
+
+            Assert.Equal(isCached,    data.IsCached);
+            Assert.Equal(expectedVol, data.Volume);
+
+            Assert.NotNull(data.WaveFormat);
+
+        }
+
+        [Theory]
+        [InlineData("TestData/AudioFiles/InvalidAudio00.mp3", true,  1.0f)]
+        [InlineData("TestData/AudioFiles/InvalidAudio00.mp3", false, 1.0f)]
+        public void TestLoadInvalidAudioFiles(string path, bool isCached, float volume)
+        {
+            Assert.Throws<InvalidDataException>(
+                () =>
+                {
+                    var data = AudioData.LoadAudio(Pathfinder.FindPathName(path), isCached, volume);
+                });
+        }
+
+        [Theory]
+        [InlineData("TestData/AudioFiles/InvalidAudio01.txt", true, 1.0f)]
+        [InlineData("TestData/AudioFiles/InvalidAudio01.txt", false, 1.0f)]
+        public void TestLoadAudioFilesUnsupportedExt(string path, bool isCached, float volume)
+        {
+            Assert.Throws<COMException>(
+                () =>
+                {
+                    var data = AudioData.LoadAudio(Pathfinder.FindPathName(path), isCached, volume);
+                });
+        }
+
+        [Theory]
+        [InlineData("TestData/AudioFiles/NotFound.mp3", true,  1.0f)]
+        [InlineData("TestData/AudioFiles/NotFound.mp3", false, 1.0f)]
+        public void TestLoadAudioFilesNotFound(string path, bool isCached, float volume)
+        {
+            Assert.Throws<FileNotFoundException>(
+                () =>
+                {
+                    var data = AudioData.LoadAudio(Pathfinder.FindPathName(path), isCached, volume);
+                });
+        }
+
+
+        [Theory]
+        [InlineData(AudioDeviceType.WASAPI)]
+        [InlineData(AudioDeviceType.Wave)]
+        [InlineData(AudioDeviceType.DirectSound)]
+        [InlineData(AudioDeviceType.ASIO)]
+        public void TestAudioDevices(AudioDeviceType deviceType)
+        {
+            var devices = AudioDevice.GetAudioDevices(AudioDataFlow.Render, deviceType);
+
+            Assert.NotNull(devices);
+
+            foreach (var item in devices)
+            {
+                Assert.NotNull(item.Id);
+                Assert.NotNull(item.Guid.ToString());
+                Assert.Equal(deviceType, item.DeviceType);
+                Assert.Equal(AudioDataFlow.Render, item.DataFlow);
+                Assert.NotNull(item.Name);
+                Assert.NotNull(item.FriendlyName);
+
+                output.WriteLine("Id: {0}",           item.Id);
+                output.WriteLine("Guid: {0}",         item.Guid);
+                output.WriteLine("DeviceType: {0}",   item.DeviceType);
+                output.WriteLine("DataFlow: {0}",     item.DataFlow);
+                output.WriteLine("Name: {0}",         item.Name);
+                output.WriteLine("FriendlyName: {0}", item.FriendlyName);
+                output.WriteLine("MMDevice: {0}",     (item.MMDevice != null));
+                output.WriteLine("----");
+            }
+        }
+
+
+
+    }
+}
---
+++ LiveSoundsSolution/Zokma.Libs/Audio/AudioDevice.cs
@@ -0,0 +1,299 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zokma.Libs.Audio
+{
+    /// <summary>
+    /// Type of Audio Device.
+    /// </summary>
+    public enum AudioDeviceType
+    {
+        /// <summary>
+        /// WASAPI.
+        /// </summary>
+        WASAPI,
+
+
+        /// <summary>
+        /// WaveOut/WaveIn.
+        /// </summary>
+        Wave,
+
+        /// <summary>
+        /// DirectSound.
+        /// </summary>
+        DirectSound,
+
+        /// <summary>
+        /// ASIO.
+        /// </summary>
+        ASIO,
+    }
+
+    public enum AudioDeviceRole
+    {
+        /// <summary>
+        /// Games and system notifications.
+        /// </summary>
+        Console,
+
+        /// <summary>
+        /// Music and movies.
+        /// </summary>
+        Multimedia,
+
+        /// <summary>
+        /// Chat and VoIP.
+        /// </summary>
+        Communications,
+    }
+
+
+    /// <summary>
+    /// Audio Device.
+    /// </summary>
+    public class AudioDevice
+    {
+        /// <summary>
+        /// Audio Device Type.
+        /// </summary>
+        public AudioDeviceType DeviceType { get; set; }
+
+        /// <summary>
+        /// Audio Data flow.
+        /// </summary>
+        public AudioDataFlow DataFlow { get; set; }
+
+        /// <summary>
+        /// Device Id. Used for initializing.
+        /// </summary>
+        public string Id { get; private set; }
+
+        /// <summary>
+        /// Device Guid. Unique id for the device.
+        /// </summary>
+        public Guid Guid { get; private set; }
+
+        /// <summary>
+        /// Device
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Friendly Name.
+        /// </summary>
+        public string FriendlyName { get; private set; }
+
+        /// <summary>
+        /// WASAPI only MMDevice.
+        /// </summary>
+        public MMDevice MMDevice { get; private set; }
+
+        /// <summary>
+        /// Gets <see cref="AudioDevice"/> from MMDevice.
+        /// </summary>
+        /// <param name="mmDevice">MMDevice.</param>
+        /// <param name="dataFlow">Audio Data flow.</param>
+        /// <returns><see cref="AudioDevice"/>.</returns>
+        private static AudioDevice GetAudioDeviceFromMMDevice(MMDevice mmDevice, AudioDataFlow dataFlow)
+        {
+            var device = new AudioDevice
+            {
+                Id           = mmDevice.ID,
+                Guid         = Guid.Empty,
+                DataFlow     = dataFlow,
+                DeviceType   = AudioDeviceType.WASAPI,
+                Name         = mmDevice.DeviceFriendlyName,
+                FriendlyName = mmDevice.FriendlyName,
+                MMDevice     = mmDevice,
+            };
+
+            if (Guid.TryParse(device.Id, out var guid))
+            {
+                device.Guid = guid;
+            }
+
+            return device;
+        }
+
+        /// <summary>
+        /// Gets WASAPI Audio device.
+        /// </summary>
+        /// <param name="dataFlow">Audio data flow.</param>
+        /// <param name="deviceRole">Audio device role.</param>
+        /// <returns>Audio device list.</returns>
+        private static AudioDevice[] GetWASAPIAudioDevices(AudioDataFlow dataFlow, AudioDeviceRole deviceRole = AudioDeviceRole.Multimedia)
+        {
+            var devices = new List<AudioDevice>();
+
+            var mmde = new MMDeviceEnumerator();
+            var role = deviceRole switch
+            {
+                AudioDeviceRole.Console => Role.Console,
+                AudioDeviceRole.Multimedia => Role.Multimedia,
+                AudioDeviceRole.Communications => Role.Communications,
+                _ => Role.Multimedia,
+            };
+
+            if (dataFlow.HasFlag(AudioDataFlow.Render))
+            {
+                try
+                {
+                    var device = mmde.GetDefaultAudioEndpoint(NAudio.CoreAudioApi.DataFlow.Render, role);
+                    devices.Add(GetAudioDeviceFromMMDevice(device, AudioDataFlow.Render));
+                }
+                catch (Exception) { }
+
+                foreach (var item in mmde.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.Render, DeviceState.Active))
+                {
+                    devices.Add(GetAudioDeviceFromMMDevice(item, AudioDataFlow.Render));
+                }
+            }
+
+            return devices.ToArray();
+        }
+
+        /// <summary>
+        /// Gets WaveOut/WaveIn audio device.
+        /// </summary>
+        /// <param name="dataFlow">Audio data flow.</param>
+        /// <returns>Audio device list.</returns>
+        private static AudioDevice[] GetWaveAudioDevices(AudioDataFlow dataFlow)
+        {
+            var devices = new List<AudioDevice>();
+
+            if (dataFlow.HasFlag(AudioDataFlow.Render))
+            {
+                for (int i = -1; i < WaveOut.DeviceCount; i++)
+                {
+                    var caps = WaveOut.GetCapabilities(i);
+
+                    devices.Add(
+                        new AudioDevice
+                        {
+                            Id = i.ToString(),
+                            Guid = caps.ProductGuid,
+                            DataFlow = AudioDataFlow.Render,
+                            DeviceType = AudioDeviceType.Wave,
+                            Name = caps.ProductName,
+                            FriendlyName = caps.ProductName,
+                        }
+                        );
+                }
+            }
+
+            return devices.ToArray();
+        }
+
+        /// <summary>
+        /// Get DirectSound audio device.
+        /// </summary>
+        /// <param name="dataFlow">Audio data flow.</param>
+        /// <returns>Audio device list.</returns>
+        private static AudioDevice[] GetDirectSoundAudioDevices(AudioDataFlow dataFlow)
+        {
+            var devices = new List<AudioDevice>();
+
+            if (dataFlow.HasFlag(AudioDataFlow.Render))
+            {
+                foreach (var item in DirectSoundOut.Devices)
+                {
+                    devices.Add(
+                        new AudioDevice
+                        {
+                            Id = item.Guid.ToString(),
+                            Guid = item.Guid,
+                            DataFlow = AudioDataFlow.Render,
+                            DeviceType = AudioDeviceType.DirectSound,
+                            Name = item.ModuleName,
+                            FriendlyName = item.Description,
+                        }
+                        );
+                }
+            }
+
+            return devices.ToArray();
+        }
+
+        /// <summary>
+        /// Gets ASIO audio device.
+        /// </summary>
+        /// <param name="dataFlow">Audio data flow.</param>
+        /// <returns>Audio device list.</returns>
+        private static AudioDevice[] GetASIOAudioDevices(AudioDataFlow dataFlow)
+        {
+            var devices = new List<AudioDevice>();
+
+            if (dataFlow.HasFlag(AudioDataFlow.Render))
+            {
+                foreach (var item in AsioOut.GetDriverNames())
+                {
+                    devices.Add(
+                        new AudioDevice
+                        {
+                            Id = item,
+                            Guid = Guid.Empty,
+                            DataFlow = AudioDataFlow.Render,
+                            DeviceType = AudioDeviceType.ASIO,
+                            Name = item,
+                            FriendlyName = item,
+                        }
+                        );
+                }
+            }
+
+            return devices.ToArray();
+        }
+
+        /// <summary>
+        /// Gets Audio devices.
+        /// </summary>
+        /// <param name="dataFlow">Audio data flow. In current version, only <see cref="AudioDataFlow.Render"/> is supported.</param>
+        /// <param name="deviceType">Audio device type.</param>
+        /// <param name="deviceRole">Audio device role.</param>
+        /// <returns>Audio device list.</returns>
+        public static AudioDevice[] GetAudioDevices(AudioDataFlow dataFlow, AudioDeviceType deviceType = AudioDeviceType.WASAPI, AudioDeviceRole deviceRole = AudioDeviceRole.Multimedia)
+        {
+            AudioDevice[] result;
+
+            if (deviceType == AudioDeviceType.WASAPI)
+            {
+                result = GetWASAPIAudioDevices(dataFlow, deviceRole);
+            }
+            else if (deviceType == AudioDeviceType.Wave)
+            {
+                result = GetWaveAudioDevices(dataFlow);
+            }
+            else if (deviceType == AudioDeviceType.DirectSound)
+            {
+                result = GetDirectSoundAudioDevices(dataFlow);
+            }
+            else if (deviceType == AudioDeviceType.ASIO)
+            {
+                result = GetASIOAudioDevices(dataFlow);
+            }
+            else
+            {
+                throw new ArgumentException(String.Format("Invalid device type: {0}", deviceType), nameof(deviceType));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets Audio render devices.
+        /// </summary>
+        /// <param name="deviceType">Audio device type.</param>
+        /// <param name="deviceRole">Audio device role.</param>
+        /// <returns>Audio device list.</returns>
+        public static AudioDevice[] GetAudioRenderDevices(AudioDeviceType deviceType = AudioDeviceType.WASAPI, AudioDeviceRole deviceRole = AudioDeviceRole.Multimedia)
+        {
+            return GetAudioDevices(AudioDataFlow.Render, deviceType, deviceRole);
+        }
+    }
+}
