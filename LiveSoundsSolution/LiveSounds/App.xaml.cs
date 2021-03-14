7f8cbbeed6597c9f9507a27c8da38f6ef5f1bfb7
Zok MADAO <74968358+zokma@users.noreply.github.com>
2021-03-15 01:59:59 +0900

    :hamster: Add LiveSounds WPF project.

---
+++ LiveSoundsSolution/LiveSounds/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Zokma.Libs;
+using Zokma.Libs.Logging;
+
+namespace LiveSounds
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private Guid instanceId = Guid.Empty;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            this.instanceId = Guid.NewGuid();
+
+            var log = new Pathfinder("Logs");
+            Log.Init(log.FindPathName("LiveSounds.log"));
+            Log.Information("OnStartup: InstanceId = {InstanceId}", instanceId.ToString());
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.Information("OnExit: InstanceId = {InstanceId}", instanceId.ToString());
+            Log.Close();
+
+            base.OnExit(e);
+        }
+    }
+}
